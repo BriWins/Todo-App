f4c7d539da902f62a0163316cab2f508b03c8c47
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-05 13:51:31 -0600

    added html
    added html elements and comments to index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+function newItem() {
+
+//adding a new item to the list
 let li = $("<li></li>");
 let inputValue = $("#input").val();
 li.append(inputValue);
@@ -8,21 +11,24 @@ if (inputValue === "") {
     $("#list").append(li);
 }
 
+//user can cross of an item
 function crossOut() {
     li.toggleClass("strike");
 }
 
-li.on("dbclick", function crossOut() {
+li.on("dblclick", function crossOut(){
     li.toggleClass("strike");
 });
 
+// adding a delete button
 let crossOutButton = $("<crossOutButton></crossOutButton>");
 crossOutButton.append(document.createTextNode("X"));
 li.append(crossOutButton);
 
 crossOutButton.on("click", deleteListItem);
 function deleteListItem() {
-    li.addClass("delete")
+    li.addClass("delete");
+     }
 }
 
 $("#list").sortable();
