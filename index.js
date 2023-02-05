45e32fe779d5617a6e47fb79c2032dac23391644
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-05 12:29:17 -0600

    updated index.js
    added basic code for testing purposes.

---
+++ index.js
@@ -0,0 +1,28 @@
+let li = $("<li></li>");
+let inputValue = $("#input").val();
+li.append(inputValue);
+
+if (inputValue === "") {
+    alert("You must write somehting!");
+} else {
+    $("#list").append(li);
+}
+
+function crossOut() {
+    li.toggleClass("strike");
+}
+
+li.on("dbclick", function crossOut() {
+    li.toggleClass("strike");
+});
+
+let crossOutButton = $("<crossOutButton></crossOutButton>");
+crossOutButton.append(document.createTextNode("X"));
+li.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
+function deleteListItem() {
+    li.addClass("delete")
+}
+
+$("#list").sortable();
